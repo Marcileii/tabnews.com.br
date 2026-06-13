c04c364261afa48d710b74f8c78205ea0aec323d
Marcilei Brandão <120384094+Marcileii@users.noreply.github.com>
2026-06-13 18:51:55 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,32 @@
 import database from "/infra/database.js";
 
-async function status(resquest, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ chave: "Alunos do curso.dev são acimas da média" });
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  const PostVersion = await database.query("SHOW server_version;");
+  const PostVersionValue = PostVersion.rows[0].server_version;
+
+  const PostVersionMax = await database.query("SHOW max_connections;");
+  const PostVersionMaxrows = PostVersionMax.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const PostVersionUsed = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpnedConnectionsValue = PostVersionUsed.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: PostVersionValue,
+        max_connections: parseInt(PostVersionMaxrows),
+        opened_connections: databaseOpnedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
